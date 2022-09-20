1565b4da937406331a7813d193223416a2247b97
disposable-kiwi <biffboffkev@gmail.com>
2022-09-20 14:22:12 -0400

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+
+    before_action :set_article, only: [:show, :edit, :destroy, :update]
+
+    def show
+        
+    end
+
+    def index
+        @all_articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article was saved successfully!"
+            redirect_to @article
+        else
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    def update
+   
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully!"
+            redirect_to @article
+        else
+            render 'edit', status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+      
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title,:description)
+    end
+end
